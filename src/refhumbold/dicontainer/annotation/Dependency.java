dea9cf94a5374631797b2b77c8bbe180946fd340
R. Kaleta <rafkaleta@onet.pl>
2018-08-22 19:10:44 +0200

    Adnotacje z @Documented

--- src/refhumbold/dicontainer/annotation/Dependency.java
+++ src/refhumbold/dicontainer/annotation/Dependency.java
@@ -1,10 +1,8 @@
 package refhumbold.dicontainer.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
 public @interface Dependency
--- src/refhumbold/dicontainer/annotation/Register.java
+++ src/refhumbold/dicontainer/annotation/Register.java
@@ -1,12 +1,10 @@
 package refhumbold.dicontainer.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 import refhumbold.dicontainer.ConstructionPolicy;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Register
