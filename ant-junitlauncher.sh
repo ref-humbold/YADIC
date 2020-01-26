c169ea5b58da2e292ad2c25cb22270f33fde0805
R. Kaleta <rafkaleta@onet.pl>
2020-01-26 21:47:46 +0100

    Fixed launcher downloading

--- ant-junitlauncher.sh
+++ ant-junitlauncher.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
-ANT_VERSION=$(ant -version | sed -r 's/.*version ([0-9.]+).*/\1/g')
-LINK="https://repo1.maven.org/maven2/org/apache/ant/ant-junitlauncher/${ANTV}/ant-junitlauncher-${ANTV}.jar"
+set -ev
 
+ANT=$(ant -version | sed -r 's/.*version ([0-9.]+).*/\1/g')
+LINK="https://repo1.maven.org/maven2/org/apache/ant/ant-junitlauncher/${ANT}/ant-junitlauncher-${ANT}.jar"
+
+echo "ant version: $ANT"
+echo "link: $LINK"
 wget $LINK -P ~/.ant/lib
