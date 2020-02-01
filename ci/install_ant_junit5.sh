c148b50e325afc6748716bf1795262a952c3c3af
R. Kaleta <rafkaleta@onet.pl>
2020-02-01 21:38:25 +0100

    Renaming files for Travis

--- ci/install_ant_junit5.sh
+++ ci/install_ant_junit5.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-echo ">>>>> Check ant-junitlauncher.jar <<<<<"
-
+echo ">>> Check ant version"
 ANT=$(ant -version | sed -r 's/.*version ([0-9.]+).*/\1/g')
 
 if [ -f ${HOME}/.ant/lib/ant-junitlauncher-${ANT}.jar ]
 then
-    echo "ant-junitlauncher.jar found :)"
+    echo ">>> File ant-junitlauncher.jar found :)"
 else
     LINK="https://repo1.maven.org/maven2/org/apache/ant/ant-junitlauncher/${ANT}/ant-junitlauncher-${ANT}.jar"
 
     rm -f ${HOME}/.ant/lib/ant-junitlauncher-*.jar
-    echo "Download ant-junitlauncher.jar (version ${ANT})"
-    wget ${LINK} -P "${HOME}/.ant/lib"
+    echo ">>> Download ant-junitlauncher.jar for version ${ANT}"
+    wget $LINK -P "${HOME}/.ant/lib"
 fi
